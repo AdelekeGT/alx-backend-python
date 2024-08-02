9bf2fbbbe7a8fe56babe2b41a192d49a2bac755e
SirJupiter <jupitertoby@gmail.com>
2024-08-02 08:33:26 +0100

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module contains type-annotated function"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function takes a float and returns the string representation of the float
+
+    Args:
+        n (float): the float to be converted to string
+
+    Returns:
+        str: the string representation of the float
+    """
+    return str(n)
