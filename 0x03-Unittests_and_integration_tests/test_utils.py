bce0c155582c63daa5695dd3c98a89131e7ccc79
SirJupiter <jupitertoby@gmail.com>
2024-08-27 22:15:12 +0100

    3. Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,13 +3,15 @@
 
 import unittest
 from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch
 
 
-def access_nested_map(nested_map, path):
-    """Retrieve the value from a nested map following the given path"""
-    for key in path:
-        nested_map = nested_map[key]
-    return nested_map
+# def access_nested_map(nested_map, path):
+#     """Retrieve the value from a nested map following the given path"""
+#     for key in path:
+#         nested_map = nested_map[key]
+#     return nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -33,3 +35,47 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
         self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """ Class for Testing Get Json """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Test for the utils.get_json function to check
+        that it returns the expected result."""
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+    """ test class to tes utils.memoize"""
+
+    def test_memoize(self):
+        """ Tests the function when calling a_property twice,
+        the correct result is returned but a_method is only
+        called once using assert_called_once
+        """
+
+        class TestClass:
+            """ Test Class for wrapping with memoize """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
