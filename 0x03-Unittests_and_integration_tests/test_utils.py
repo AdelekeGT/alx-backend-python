ede23a43daac44e8a4f4df6a7a1a36ffd957d173
SirJupiter <jupitertoby@gmail.com>
2024-08-27 22:08:52 +0100

    1. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,10 +25,11 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        """Test that KeyError is raised for invalud paths"""
-        with self.assertRaises(KeyError):
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test that KeyError is raised for invalid paths"""
+        with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
